4a74c9ba91fb80e76385b929e760e07bcea64a54
muthamilselvi410 <muthamilselvi410@gmail.com>
2026-05-10 19:31:19 +0530

    Fixed Render deployment

--- kani_medical/settings.py
+++ kani_medical/settings.py
@@ -4,8 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kani-medical-secret-key-2024'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['kani-medical.onrender.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -24,7 +23,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'kani_medical.urls'
 
